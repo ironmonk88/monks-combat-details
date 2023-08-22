6551b6f6fdf0afa492d71ddb526bc4e2b5ab48e1
ironmonk88 <75920956+ironmonk88@users.noreply.github.com>
2023-08-21 21:47:43 -0700

    11.06 changes

--- monks-combat-details.js
+++ monks-combat-details.js
@@ -36,13 +36,20 @@ export let combatposition = () => {
 };
 
 export let patchFunc = (prop, func, type = "WRAPPER") => {
-    if (game.modules.get("lib-wrapper")?.active) {
-        libWrapper.register("monks-combat-details", prop, func, type);
-    } else {
+    let nonLibWrapper = () => {
         const oldFunc = eval(prop);
         eval(`${prop} = function (event) {
             return func.call(this, oldFunc.bind(this), ...arguments);
         }`);
+    }
+    if (game.modules.get("lib-wrapper")?.active) {
+        try {
+            libWrapper.register("monks-combat-details", prop, func, type);
+        } catch (e) {
+            nonLibWrapper();
+        }
+    } else {
+        nonLibWrapper();
     }
 }
 
